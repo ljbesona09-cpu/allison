28cc559fa58abcf15c646ec86ca7b6c07096daf5
ljbesona09-cpu <ljbesona09@gmail.com>
2026-02-13 20:40:55 +0800

    Comment

---
+++ function showImage() {.js
@@ -0,0 +1,11 @@
+function showImage() {
+  const img = document.getElementById("myImage");
+ 
+  if (img.style.display === "none") {
+    img.style.display = "block";
+  }  
+  
+  else {
+    img.style.display = "none";
+  }
+}
